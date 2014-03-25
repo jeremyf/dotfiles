02ebbb0f285ca84834191f03bf37d0bf442b0221
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-24 21:46:40 -0400

    Adjusting PS1 behavior

--- system/ps1.bash
+++ system/ps1.bash
@@ -1,51 +1,62 @@
-GIT_PS1_SHOWDIRTYSTATE=true
-GIT_PS1_SHOWSTASHSTATE=true
-GIT_PS1_SHOWUNTRACKEDFILES=true
-GIT_PS1_SHOWUPSTREAM="auto"
-
+# Make sure git-prompt is available, otherwise this PS1 change will be very
+# boring.
 if [ -a `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+
+  GIT_PS1_SHOWDIRTYSTATE=true
+  GIT_PS1_SHOWSTASHSTATE=true
+  GIT_PS1_SHOWUNTRACKEDFILES=true
+  GIT_PS1_SHOWUPSTREAM="auto"
   source `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+
+  # No way of knowing for certain if rvm-prompt is available.
+  __rvm_ps1() {
+    if [ -a $HOME/.rvm/bin/rvm-prompt ]; then
+      echo " $($HOME/.rvm/bin/rvm-prompt v p g) "
+    else
+      echo ' '
+    fi
+  }
+
+  # via http://tammersaleh.com/posts/a-better-rvm-bash-prompt
+  bash_prompt() {
+    local NONE="\[\033[0m\]"    # unsets color to term's fg color
+
+    # regular colors
+    local K="\[\033[0;30m\]"    # black
+    local R="\[\033[0;31m\]"    # red
+    local G="\[\033[0;32m\]"    # green
+    local Y="\[\033[0;33m\]"    # yellow
+    local B="\[\033[0;34m\]"    # blue
+    local M="\[\033[0;35m\]"    # magenta
+    local C="\[\033[0;36m\]"    # cyan
+    local W="\[\033[0;37m\]"    # white
+
+    # emphasized (bolded) colors
+    local EMK="\[\033[1;30m\]"
+    local EMR="\[\033[1;31m\]"
+    local EMG="\[\033[1;32m\]"
+    local EMY="\[\033[1;33m\]"
+    local EMB="\[\033[1;34m\]"
+    local EMM="\[\033[1;35m\]"
+    local EMC="\[\033[1;36m\]"
+    local EMW="\[\033[1;37m\]"
+
+    # background colors
+    local BGK="\[\033[40m\]"
+    local BGR="\[\033[41m\]"
+    local BGG="\[\033[42m\]"
+    local BGY="\[\033[43m\]"
+    local BGB="\[\033[44m\]"
+    local BGM="\[\033[45m\]"
+    local BGC="\[\033[46m\]"
+    local BGW="\[\033[47m\]"
+
+    local UC=$W                 # user's color
+    [ $UID -eq "0" ] && UC=$R   # root's color
+
+    PS1="$G\w$R\$(__git_ps1)$B\$(__rvm_ps1)${NONE}$ "
+  }
+
+  bash_prompt
+  unset bash_prompt
 fi
-
-# via http://tammersaleh.com/posts/a-better-rvm-bash-prompt
-bash_prompt() {
-  local NONE="\[\033[0m\]"    # unsets color to term's fg color
-
-  # regular colors
-  local K="\[\033[0;30m\]"    # black
-  local R="\[\033[0;31m\]"    # red
-  local G="\[\033[0;32m\]"    # green
-  local Y="\[\033[0;33m\]"    # yellow
-  local B="\[\033[0;34m\]"    # blue
-  local M="\[\033[0;35m\]"    # magenta
-  local C="\[\033[0;36m\]"    # cyan
-  local W="\[\033[0;37m\]"    # white
-
-  # emphasized (bolded) colors
-  local EMK="\[\033[1;30m\]"
-  local EMR="\[\033[1;31m\]"
-  local EMG="\[\033[1;32m\]"
-  local EMY="\[\033[1;33m\]"
-  local EMB="\[\033[1;34m\]"
-  local EMM="\[\033[1;35m\]"
-  local EMC="\[\033[1;36m\]"
-  local EMW="\[\033[1;37m\]"
-
-  # background colors
-  local BGK="\[\033[40m\]"
-  local BGR="\[\033[41m\]"
-  local BGG="\[\033[42m\]"
-  local BGY="\[\033[43m\]"
-  local BGB="\[\033[44m\]"
-  local BGM="\[\033[45m\]"
-  local BGC="\[\033[46m\]"
-  local BGW="\[\033[47m\]"
-
-  local UC=$W                 # user's color
-  [ $UID -eq "0" ] && UC=$R   # root's color
-
-  PS1="$G# \w$R\$(__git_ps1) $B\$(~/.rvm/bin/rvm-prompt v p g)${NONE} $ "
-}
-
-bash_prompt
-unset bash_prompt
