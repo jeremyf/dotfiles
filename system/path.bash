e6ab257b56beb778bb13d22eff211f85c1bb2be9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-09-30 13:47:44 -0400

    Adding /usr/local/sbin to the path

--- system/path.bash
+++ system/path.bash
@@ -1,6 +1,9 @@
 if [ -d $HOME/bin ]; then
   echo "$PATH" | grep -q "$HOME/bin:" || export PATH="$HOME/bin:$PATH"
 fi
+if [ -d /usr/local/sbin ]; then
+  echo "$PATH" | grep -q "/usr/local/sbin:" || export PATH="/usr/local/sbin:$PATH"
+fi
 if [ -d $DOTFILES/bin ]; then
   echo "$PATH" | grep -q "$DOTFILES/bin:" || export PATH="$DOTFILES/bin:$PATH"
 fi
