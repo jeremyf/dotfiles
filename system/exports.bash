53a4bdb4618d60afc3f648ad64ccd0a7e789aeef
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-08-05 13:44:22 -0400

    Ensure OPEN-SSL is set

--- system/exports.bash
+++ system/exports.bash
@@ -3,6 +3,8 @@ export EVENT_NOKQUEUE=1
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='7;31'
 export PWS="$HOME/.safe/pws"
-export VISUAL='atom --wait'
+export VISUAL='atom --wait -n --clear-window-state'
 export EDITOR=$VISUAL
+# So mysql will build with OPEN-SSL
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
 # export RUBYLIB=".:test:$RUBYLIB"
