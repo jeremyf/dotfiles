22bee42e9b4d2e603d133d0bf8e86e191bf90b16
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-08-16 11:08:08 -0400

    Improving history behavior
    https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows/1292#1292

--- system/exports.bash
+++ system/exports.bash
@@ -5,6 +5,17 @@ export GREP_COLOR='7;31'
 export PWS="$HOME/.safe/pws"
 export VISUAL='atom --wait -n --clear-window-state'
 export EDITOR=$VISUAL
+
+# Avoid duplicates in history
+export HISTCONTROL=ignoredups:erasedups
+
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+
 # So mysql will build with OPEN-SSL
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
 # export RUBYLIB=".:test:$RUBYLIB"
+export IGNOREEOF=1
